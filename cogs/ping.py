38871b7fb3391bc2ba893de6e52f5ab069bf1873
Sαr Kαr <101462005+Sarkar069@users.noreply.github.com>
2024-06-14 21:21:12 +0530

    Update ping.py

--- cogs/ping.py
+++ cogs/ping.py
@@ -4,8 +4,9 @@ import asyncio
 
 
 bot = commands.InteractionBot(intents=disnake.Intents.all())
-url = "https://cdn.discordapp.com/attachments/1101410251061866590/1139508274367045632/bot_logo.png"
+url = "" #put here your bot logo URL
 
+# this is very necessary for cog
 class PingCommand(commands.Cog):
 
     def __init__(self, bot: commands.InteractionBot):
@@ -14,10 +15,10 @@ class PingCommand(commands.Cog):
     @commands.slash_command(description="Get the bot's current websocket latency.")
     @commands.guild_only()
     async def ping(self, ctx: disnake.ApplicationCommandInteraction):
-        await ctx.response.defer(ephemeral=True)
+        await ctx.response.defer(ephemeral=True) # if true only who ran the command can see the message bot send
         await asyncio.sleep(3)
         embed = disnake.Embed(title=f" Pong..! 🏓 `{round(self.bot.latency * 1000)}` ms", color=disnake.Colour.purple())
-        embed.set_author(name="Stellar Enigma", icon_url=url)
+        embed.set_author(name="Your Bot name", icon_url=url)
         await ctx.edit_original_response(embed=embed)
         
         
@@ -28,3 +29,5 @@ class PingCommand(commands.Cog):
 
 def setup(bot: commands.InteractionBot):
     bot.add_cog(PingCommand(bot))
+
+# adds your cog to bot
